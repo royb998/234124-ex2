06b6b27811711182270127f1f6b73b6991ec79d5
Roy Bar-On <royb997@gmail.com>
2022-12-21 23:14:20 +0200

    Added basic functionality for HP module

--- HealthPoints.cpp
+++ HealthPoints.cpp
@@ -3,3 +3,114 @@
 //
 
 #include "HealthPoints.h"
+
+HealthPoints::HealthPoints(int maxHp)
+{
+    if (maxHp <= 0)
+    {
+        throw HealthPoints::InvalidArgument();
+    }
+
+    this->m_maxHP = maxHp;
+    this->m_currentHP = maxHp;
+}
+
+HealthPoints::HealthPoints(int maxHp, int currentHP)
+{
+    if (maxHp <= 0)
+    {
+        throw HealthPoints::InvalidArgument();
+    }
+
+    this->m_maxHP = maxHp;
+    this->setHP(currentHP);
+}
+
+void HealthPoints::setHP(int value)
+{
+    if (value > this->m_maxHP)
+    {
+        value = this->m_maxHP;
+    }
+    if (value < 0)
+    {
+        value = 0;
+    }
+
+    this->m_currentHP = value;
+}
+
+/* Arithmetic operators */
+
+HealthPoints HealthPoints::operator+(int value)
+{
+    return HealthPoints(this->m_maxHP,
+                        this->m_currentHP + value);
+}
+
+HealthPoints operator+(int value, const HealthPoints& hp)
+{
+    return HealthPoints(hp.m_maxHP,
+                        hp.m_currentHP + value);
+}
+
+HealthPoints HealthPoints::operator-(int value)
+{
+    return HealthPoints(this->m_maxHP,
+                        this->m_currentHP - value);
+}
+
+HealthPoints operator-(int value, const HealthPoints& hp)
+{
+    return HealthPoints(hp.m_maxHP,
+                        hp.m_currentHP - value);
+}
+
+void HealthPoints::operator+=(int value)
+{
+    this->setHP(this->m_currentHP + value);
+}
+
+void HealthPoints::operator-=(int value)
+{
+    this->setHP(this->m_currentHP - value);
+}
+
+/* Comparison operators */
+
+bool HealthPoints::operator==(HealthPoints other)
+{
+    return this->m_currentHP == other.m_currentHP;
+}
+
+bool HealthPoints::operator!=(HealthPoints other)
+{
+    return this->m_currentHP != other.m_currentHP;
+}
+
+bool HealthPoints::operator>(HealthPoints other)
+{
+    return this->m_currentHP > other.m_currentHP;
+}
+
+bool HealthPoints::operator<(HealthPoints other)
+{
+    return this->m_currentHP < other.m_currentHP;
+}
+
+bool HealthPoints::operator>=(HealthPoints other)
+{
+    return this->m_currentHP >= other.m_currentHP;
+}
+
+bool HealthPoints::operator<=(HealthPoints other)
+{
+    return this->m_currentHP <= other.m_currentHP;
+}
+
+/* IO operators */
+
+std::ostream& operator<<(std::ostream& os, const HealthPoints& hp)
+{
+    return os << hp.m_currentHP << "(" << hp.m_maxHP << ")";
+}
--- HealthPoints.h
+++ HealthPoints.h
@@ -1,16 +1,46 @@
 //
-// Created by royb9 on 21/12/2022.
+// Created by Roy Bar-On on 21/12/2022.
 //
 
 #ifndef _HEALTHPOINTS_H
 #define _HEALTHPOINTS_H
 
+#include <iostream>
 
+class HealthPoints
+{
+private:
+    const static int DEFAULT_MAX_HP = 100;
 
-class HealthPoints {
+    int m_maxHP;
+    int m_currentHP;
 
+    HealthPoints(int maxHp, int currentHP);
+
+    void setHP(int value);
+
+public:
+    HealthPoints(int maxHp=DEFAULT_MAX_HP);
+
+    HealthPoints operator+(int value);
+    HealthPoints operator-(int value);
+    friend HealthPoints operator+(int value, const HealthPoints& hp);
+    friend HealthPoints operator-(int value, const HealthPoints& hp);
+    void operator+=(int value);
+    void operator-=(int value);
+
+    bool operator==(HealthPoints other);
+    bool operator!=(HealthPoints other);
+    bool operator>(HealthPoints other);
+    bool operator<(HealthPoints other);
+    bool operator>=(HealthPoints other);
+    bool operator<=(HealthPoints other);
+
+    friend std::ostream& operator<<(std::ostream& os, const HealthPoints& hp);
+
+    /* Exceptions */
+    class InvalidArgument{};
 };
-
 
 
 #endif //_HEALTHPOINTS_H
