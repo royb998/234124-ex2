c1c8b4e87772e060cb5722e8ed17fe2d70964ae6
Roy Bar-On <royb997@gmail.com>
2022-12-21 23:13:55 +0200

    Added clean files for HP module

---
+++ HealthPoints.cpp
@@ -0,0 +1,5 @@
+//
+// Created by royb9 on 21/12/2022.
+//
+
+#include "HealthPoints.h"
---
+++ HealthPoints.h
@@ -0,0 +1,16 @@
+//
+// Created by royb9 on 21/12/2022.
+//
+
+#ifndef _HEALTHPOINTS_H
+#define _HEALTHPOINTS_H
+
+
+
+class HealthPoints {
+
+};
+
+
+
+#endif //_HEALTHPOINTS_H
